291474a9c548baf762c9cc5862d70ea9f6003ed1
Phil <p.rondinone@web.de>
2021-06-05 12:41:30 +0200

    Aufgabe 1 komplett

--- Übungsblatt04/vector.cpp
+++ Übungsblatt04/vector.cpp
@@ -1,8 +1,32 @@
 #include <vector>
 #include <iostream>
 #include <utility>
-
-std::pair<double,double> minmax_func(std::vector<double>& n)
+#include <cmath>
+void reversed_swap(std::vector<double>& v)
+{
+	for(int i = 0; i < v.size()/2; i++)
+	{
+	std::swap(v[i],v[(v.size()-1)-i]);
+	}
+}
+void rounded(std::vector<double>& v)
+{
+	for(int i = 0; i < v.size(); i++)
+	{
+		v[i] = std::round(v[i]);
+	}
+}
+std::vector<double> reversed(const std::vector<double>& v)
+{
+	std::vector<double> reversedvector = v;
+	
+		for(int i = 0; i < v.size(); i++)
+		{	
+			reversedvector[i] = v[(v.size()-1)-i];
+		}
+return reversedvector;
+}
+std::pair<double,double> minmax_func(std::vector<double> n)
 {
 	double min = n[0];
 	double max = n[0];
@@ -25,7 +49,7 @@ minmax = std::make_pair(min,max);
 return minmax;	
 }
 
-void ausgabe(std::vector<double>& n)
+void ausgabe(std::vector<double> n)
 {
 	for(int i=0; i < n.size(); i++)
 	{
@@ -39,18 +63,39 @@ int main(int argc, char** argv)
 {
 	std::vector<double> v1;
 	std::vector<double> v2(10);
-	std::vector<double> v3 = { 4, 4.3, 5.5, 6.7, 2, 9.4 };
-
+	std::vector<double> v3 = { 4, 4.3, 5.5, 6.7, 2, 9.4, 10 }; // ungerader vector
+	std::vector<double> v4 = { 5, 2.743, 1.23, 87, 99, 72.5 }; // gerader vector
+	
 std::cout << "Ausgabe von v1:" << std::endl; // Aufgabe 1 a)
 ausgabe(v1);
 std::cout << "Ausgabe von v2:" << std::endl;
 ausgabe(v2); // Werte ohne Zuweisung bekommen den Wert 0
 std::cout << "Ausgabe von v3:" << std::endl;
 ausgabe(v3);
+std::cout << "Ausgabe von v4:" << std::endl;
+ausgabe(v4);
 
 std::pair<double,double> minmax_loesung = minmax_func(v3); // Aufgabe 1 b)
 std::cout << "Min: " << minmax_loesung.first << std::endl;
 std::cout << "Max: " << minmax_loesung.second << std::endl;
+
+std::cout << "Ausgabe von reversed vector: " << std::endl;
+ausgabe(reversed(v3));
+ausgabe(reversed(v1));
+ausgabe(reversed(v2));
+ausgabe(reversed(v4));
+
+std::cout << "Ausgabe von rounded vector: " << std::endl;
+rounded(v3);
+ausgabe(v3);
+rounded(v4);
+ausgabe(v4);
+
+std::cout << "Ausgabe von reversed_swap vector: " << std::endl;
+reversed_swap(v3);
+ausgabe(v3);
+reversed_swap(v4);
+ausgabe(v4);
 }
 	
 	
